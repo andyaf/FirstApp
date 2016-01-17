cb156bb8c3f91bd02eaed523d61d0cc5506df4cf
Andrew2 <andrew2@Andrews-MacBook-Pro-2.local>
2016-01-17 16:19:28 -0500

    First Commit for super cool

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainPic: UIImageView!
+    
+    @IBAction func mainButton(sender: AnyObject) { mainPic.hidden = false
+        firstClick.hidden = true
+    }
+    
+    @IBOutlet weak var firstClick: UIButton!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
